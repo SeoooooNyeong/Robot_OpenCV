98fe045beefe752a98f94c83f456a83b06aca3be
SeoooooNyeong <113419106+SeoooooNyeong@users.noreply.github.com>
2023-10-22 19:39:16 +0900

    Add files via upload

---
+++ SSPro/CmdDefine.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define M_NONE 0
+#define M_FORWARD 10
+#define M_BACKWARD 11
+#define M_SIDE_LEFT 12
+#define M_SIDE_RIGHT 13
+#define M_TURN_LEFT 14
+#define M_TURN_RIGHT 15
+#define M_TEST 16
+#define CMD_M_NONE ";"
+#define CMD_M_FORWARD "JK0001;"
+#define CMD_M_BACKWARD "JK0008;"
+#define CMD_M_SIDE_LEFT "JK0080;"
+#define CMD_M_SIDE_RIGHT "JK1000;"
+#define CMD_M_TURN_LEFT "JK2000;"
+#define CMD_M_TURN_RIGHT "JK3000;"
+#define CMD_M_TEST "JR0002;"
